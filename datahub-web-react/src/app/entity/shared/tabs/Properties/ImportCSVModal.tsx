1d4b0cef76a811e4d0c4b414b3ef5859f9d6dcd5
tantm <tan.thai@techxcorp.com>
2022-07-12 10:14:03 +0000

    Update search

--- datahub-web-react/src/app/entity/shared/tabs/Properties/ImportCSVModal.tsx
+++ datahub-web-react/src/app/entity/shared/tabs/Properties/ImportCSVModal.tsx
@@ -3,12 +3,12 @@ import { message, Button, Modal } from 'antd';
 // import styled from 'styled-components/macro';
 // import { useUpdateCustomPropertiesMutation } from '../../../../../graphql/mutations.generated';
 // import { StringMapEntryInput } from '../../../../../types.generated';
+import { EntityType } from '../../../../../types.generated';
 // import { useEntityRegistry } from '../../../../useEntityRegistry';
 // import { useEntityData } from '../../EntityContext';
 import PropertiesTermsTable, { PropertyDataType } from './PropertiesTermsTable';
-
+import { useGetSearchResultsForMultipleQuery } from '../../../../../graphql/search.generated';
 // import analytics, { EventType, EntityActionType } from '../../../../analytics';
-
 interface Props {
     onClose: () => void;
 }
@@ -17,10 +17,26 @@ export default function ImportCSVModal(props: Props) {
     const { onClose } = props;
     const customPropsInit: PropertyDataType[] = [];
     const [dataSource, setDataSource] = useState(customPropsInit);
+    const [createButtonDisabled, setCreateButtonDisabled] = useState(true);
 
-    function createGlossaryFromCSV(){
+    function CreateGlossaryFromCSV() {
         message.loading({ content: 'Saving new term groups and terms...' });
-        
+        setCreateButtonDisabled(false);
+        const query = '';
+        const { data } = useGetSearchResultsForMultipleQuery({
+            variables: {
+                input: {
+                    types: [EntityType.GlossaryTerm, EntityType.GlossaryNode],
+                    query,
+                    start: 0,
+                    count: 500,
+                },
+            },
+            skip: !query,
+        });
+        console.log('Sear all data: ', data);
+        message.destroy();
+        setCreateButtonDisabled(true);
     }
 
     return (
@@ -34,7 +50,9 @@ export default function ImportCSVModal(props: Props) {
                     <Button onClick={onClose} type="text">
                         Cancel
                     </Button>
-                    <Button onClick={createGlossaryFromCSV}>Create</Button>
+                    <Button onClick={CreateGlossaryFromCSV} disabled={createButtonDisabled}>
+                        Create
+                    </Button>
                 </>
             }
         >
