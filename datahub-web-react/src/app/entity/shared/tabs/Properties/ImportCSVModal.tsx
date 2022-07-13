3b24b87b65ed9c0c3a2047bb6fa1c5ea236f44e2
tantm <tan.thai@techxcorp.com>
2022-07-13 11:35:46 +0000

    tmp commit

--- datahub-web-react/src/app/entity/shared/tabs/Properties/ImportCSVModal.tsx
+++ datahub-web-react/src/app/entity/shared/tabs/Properties/ImportCSVModal.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { message, Button, Modal } from 'antd';
+import MD5 from 'crypto-js/md5';
+import md5ToUuid from 'md5-to-uuid';
 // import styled from 'styled-components/macro';
 // import { useUpdateCustomPropertiesMutation } from '../../../../../graphql/mutations.generated';
 // import { StringMapEntryInput } from '../../../../../types.generated';
@@ -8,7 +10,10 @@ import { EntityType } from '../../../../../types.generated';
 // import { useRefetch } from '../../EntityContext';
 import PropertiesTermsTable, { PropertyDataType } from './PropertiesTermsTable';
 import { useGetSearchResultsForMultipleQuery } from '../../../../../graphql/search.generated';
-// import { useGetGlossaryTermQuery } from '../../../../../graphql/glossaryTerm.generated';
+import {
+    useCreateGlossaryTermMutation,
+    useCreateGlossaryNodeMutation,
+} from '../../../../../graphql/glossaryTerm.generated';
 
 // import analytics, { EventType, EntityActionType } from '../../../../analytics';
 interface Props {
@@ -22,6 +27,14 @@ function ImportCSVModal(props: Props) {
     const [importButtonDisabled, setimportButtonDisabled] = useState(false);
     const [searchResults, setSearchResults] = useState<any[]>([]);
     // const refetch = useRefetch();
+    const [createGlossaryTermMutation] = useCreateGlossaryTermMutation();
+    const [createGlossaryNodeMutation] = useCreateGlossaryNodeMutation();
+
+    function getUUID(value: str) {
+        const md5Hash = MD5(value).toString();
+        const uuid = md5ToUuid(md5Hash);
+        return uuid;
+    }
 
     const { data } = useGetSearchResultsForMultipleQuery({
         variables: {
@@ -40,19 +53,92 @@ function ImportCSVModal(props: Props) {
         if (typeof temp === 'object') setSearchResults(temp);
     }
 
-    console.log('searchResults: ', searchResults);
-    console.log('dataSource', dataSource);
+    function createGlossaryEntity(type: entityType, inputValue: Object) {
+        const mutation =
+            entityType === EntityType.GlossaryTerm ? createGlossaryTermMutation : createGlossaryNodeMutation;
+
+        mutation({
+            variables: {
+                input: inputValue,
+            },
+        })
+            .catch((e) => {
+                message.destroy();
+                message.error({
+                    content: `Failed to create: \n ${e.message || ''}`,
+                    duration: 3,
+                });
+            })
+            .finally(() => {
+                message.loading({
+                    content: 'Updating ',
+                    duration: 2,
+                });
+                setTimeout(() => {
+                    message.success({
+                        content: `Created ${entityRegistry.getEntityName(entityType)}!`,
+                        duration: 2,
+                    });
+                }, 2000);
+            });
+    }
 
     function importGlossaryEntity() {
         setimportButtonDisabled(true);
         message.loading({ content: 'Saving new glossarys and terms...' });
         console.log('searchResults: ', searchResults);
-        // var dict = {
-        //     "GLOSSARY_TERM": {},
-        //     "GLOSSARY_NODE": {}
-        // }
+        var dict = {
+            GLOSSARY_TERM: {},
+            GLOSSARY_NODE: {},
+        };
+        searchResults.forEach((e) => {
+            if (e.entity.type === 'GLOSSARY_TERM') {
+                dict['GLOSSARY_TERM'][e.entity.properties.name] = e.entity.urn;
+            } else if (e.entity.type === 'GLOSSARY_NODE') {
+                dict['GLOSSARY_NODE'][e.entity.properties.name] = e.entity.urn;
+            }
+        });
+        var glossaryInsert = {
+            GLOSSARY_TERM: {},
+            GLOSSARY_NODE: {},
+        };
+        dataSource.forEach((e) => {
+            if (e.term_gourp_1 && e.term_gourp_2 && e.term_gourp_3) {
+                e['term_gourp_1_id'] = getUUID(e.term_gourp_1);
+                e['term_gourp_2_id'] = getUUID(e.term_gourp_1 + e.term_gourp_2);
+                e['term_gourp_3_id'] = getUUID(e.term_gourp_1 + e.term_gourp_2 + e.term_gourp_3);
+                e['term_id'] = getUUID(e.term_gourp_1 + e.term_gourp_2 + e.term_gourp_3 + e.term);
+                glossaryInsert['GLOSSARY_NODE'][e['term_gourp_1_id']] = {
+                    id: e['term_gourp_1_id'],
+                    name: e['term_gourp_1'],
+                    parentNode: null,
+                };
+                glossaryInsert['GLOSSARY_NODE'][e['term_gourp_2_id']] = {
+                    id: e['term_gourp_2_id'],
+                    name: e['term_gourp_2'],
+                    parentNode: 'urn:li:glossaryNode:' + e['term_gourp_1_id'],
+                };
+                glossaryInsert['GLOSSARY_NODE'][e['term_gourp_3_id']] = {
+                    id: e['term_gourp_3_id'],
+                    name: e['term_gourp_3'],
+                    parentNode: 'urn:li:glossaryNode:' + e['term_gourp_2_id'],
+                };
+                glossaryInsert['GLOSSARY_TERM'][e['term_id']] = {
+                    id: e['term_id'],
+                    name: e['term'],
+                    parentNode: 'urn:li:glossaryNode:' + e['term_gourp_3_id'],
+                };
+            }
+        });
+        Object.entries(glossaryInsert['GLOSSARY_NODE']).forEach(([k, v]) => {
+            createGlossaryEntity(EntityType.GlossaryTerm, v);
+        });
+
+        Object.entries(glossaryInsert['GLOSSARY_TERM']).forEach(([k, v]) => {
+            createGlossaryEntity(EntityType.GlossaryNode, v);
+        });
         setimportButtonDisabled(false);
-        onClose();
+        // onClose();
     }
 
     return (
